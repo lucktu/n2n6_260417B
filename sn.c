58097a0f55f942535f4ad4e0eb6227efdd9c4911
lucktu <67133690@qq.com>
2026-04-29 04:18:38 +0000

    Restore display of full community

--- sn.c
+++ sn.c
@@ -933,38 +933,6 @@ static int try_forward( n2n_sn_t * sss,
     return 0;
 }
 
-
-/* Hide community name for privacy:
- * - 1 char: "*" 
- * - 2-7 chars: show first half + "*"
- * - 8+ chars: show first 4 + "*"
- */
-static void hide_community_name(char *dst, const char *src, size_t dst_size) {
-    size_t len = strlen(src);
-    size_t show_len;
-    
-    if (len == 0) {
-        dst[0] = '\0';
-        return;
-    }
-    
-    if (len == 1) {
-        show_len = 0;
-    } else if (len < 8) {
-        show_len = len / 2;
-    } else {
-        show_len = 4;
-    }
-    
-    if (show_len >= dst_size) show_len = dst_size - 2;
-    
-    if (show_len > 0) {
-        memcpy(dst, src, show_len);
-    }
-    dst[show_len] = '*';
-    dst[show_len + 1] = '\0';
-}
-
 /** Try and broadcast a message to all edges in the community.
  *
  *  This will send the exact same datagram to zero or more edges registered to
@@ -1045,8 +1013,7 @@ static int process_mgmt( n2n_sn_t * sss,
     uint32_t displayed_edges = 0;
     for (int i = 0; i < num_communities; i++) {
         /* Community name line with traffic stats on same line */
-        char hidden_name[64];
-        hide_community_name(hidden_name, (char*)communities[i], sizeof(hidden_name));
+        char *comm_name = (char*)communities[i];
         
         if (sss->traffic_stats_enabled) {
             struct community_stats *cs = sss->comm_stats;
@@ -1059,12 +1026,12 @@ static int process_mgmt( n2n_sn_t * sss,
                 const char *arrow = (cs->instant_Bps > 0) ? "--->" : "    ";
                 ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE,
                                    "%-57s  %s %-7.1f  %-7.1f  %-10.1f\n",
-                                   hidden_name, arrow, kbps, gb_24h, gb_30d);
+                                   comm_name, arrow, kbps, gb_24h, gb_30d);
             } else {
-                ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE, "%s\n", hidden_name);
+                ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE, "%s\n", comm_name);
             }
         } else {
-            ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE, "%s\n", hidden_name);
+            ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE, "%s\n", comm_name);
         }
         r = sendto(sss->mgmt_sock, resbuf, ressize, 0, sender_sock, sender_sock_len);
         if (r <= 0) return -1;
@@ -1141,8 +1108,7 @@ static int process_mgmt( n2n_sn_t * sss,
                 time_t idle = now - cs->last_active;
                 if (idle < 86400) {
                     /* Offline < 24h: show individually */
-                    char hidden_name[64];
-                    hide_community_name(hidden_name, (char*)cs->community_name, sizeof(hidden_name));
+                    char *comm_name = (char*)cs->community_name;
                     if (cs->total_30d > 0) {
                         double kbps  = cs->instant_Bps / 1024.0;
                         double gb24h = cs->last_24h_bytes / (1024.0*1024.0*1024.0);
@@ -1150,10 +1116,10 @@ static int process_mgmt( n2n_sn_t * sss,
                         const char *arrow = (cs->instant_Bps > 0) ? "--->" : "    ";
                         ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE,
                                            "%-57s  %s %-7.1f  %-7.1f  %-10.1f\n",
-                                           hidden_name, arrow, kbps, gb24h, gb30d);
+                                           comm_name, arrow, kbps, gb24h, gb30d);
                     } else {
                         ressize = snprintf(resbuf, N2N_SN_PKTBUF_SIZE, "%s\n",
-                                           hidden_name);
+                                           comm_name);
                     }
                     r = sendto(sss->mgmt_sock, resbuf, ressize, 0, sender_sock, sender_sock_len);
                     if (r <= 0) return -1;
